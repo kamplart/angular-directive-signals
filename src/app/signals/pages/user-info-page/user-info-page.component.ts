2d131bf125d627e57cc14f812b9c29676f4b1427
CamiloAir <dev1@e-advising.biz>
2023-11-01 22:18:28 -0500

    Fin seccion 24

---
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
+
+import { User } from '../../interfaces/user-request.interface';
+import { UsersServiceService } from '../../services/user-service.service';
+
+@Component({
+  templateUrl: './user-info-page.component.html',
+  styleUrls: ['./user-info-page.component.css']
+})
+export class UserInfoPageComponent implements OnInit {
+
+  private usersService = inject(UsersServiceService);
+  public userId = signal(1);
+
+  public currentUser = signal<User|undefined>(undefined);
+  public userWasFound = signal(true);
+
+  public fullName = computed<string>( () => {
+    if ( !this.currentUser() ) return 'Usuario no encontrado';
+    return `${ this.currentUser()?.first_name } ${ this.currentUser()?.last_name }`;
+  });
+
+
+  ngOnInit(): void {
+    this.loadUser( this.userId() )
+  }
+
+  loadUser( id: number ) {
+    if ( id <= 0 ) return;
+
+    this.userId.set(id);
+    this.currentUser.set(undefined);
+
+    this.usersService.getUserById( id )
+      .subscribe({
+        next: (user) => {
+         this.currentUser.set( user );
+         this.userWasFound.set(true);
+        },
+        error: () => {
+          this.userWasFound.set(false);
+          this.currentUser.set(undefined);
+        },
+      });
+
+
+  }
+
+}
